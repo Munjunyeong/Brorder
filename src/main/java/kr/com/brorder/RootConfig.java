7e82d0c644290b870e00a1262bbb26e69fb2890a
문준영 <p01057119647@gmail.com>
2026-06-15 18:52:11 +0900

    유저 인터셉터 생성

---
+++ src/main/java/kr/com/brorder/RootConfig.java
@@ -0,0 +1,32 @@
+package kr.com.brorder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RootConfig implements WebMvcConfigurer{
+	
+	@Value("${kopo.upload.url}")
+	private String uploadUrl;
+	
+	@Autowired
+	HandlerInterceptor userInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(userInterceptor).addPathPatterns("/users/**");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+		.addResourceHandler("/upload/**")
+		.addResourceLocations(uploadUrl);
+	}
+
+}
---
+++ src/main/java/kr/com/brorder/users/UserInterceptor.java
@@ -0,0 +1,30 @@
+package kr.com.brorder.users;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class UserInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		
+		Users users = (Users) session.getAttribute("users");
+		
+		if(users != null) {
+			String role = users.getRole().toUpperCase();
+		
+			if(role.equals("USER"))
+				return true;
+		}
+		
+		response.sendRedirect("/login");
+		return false;
+	}
+
+}
